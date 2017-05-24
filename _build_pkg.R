4bc5c607fddb5fd31c8ca7bcafd94f0727eecb32
Walmes Zeviani <walmes@ufpr.br>
2017-05-24 13:09:35 -0300

    Uses trusty in Travis and improves build_pkg.

--- _build_pkg.R
+++ _build_pkg.R
@@ -3,6 +3,9 @@
 
 library(devtools)
 
+# Delete to recreate files.
+system("git clean -xf ./src")
+
 load_all()
 packageVersion("gammacount")
 ls("package:gammacount")
@@ -23,12 +26,14 @@ build(vignettes = FALSE, manual = TRUE, path = "./docs")
 library(pkgdown)
 
 build_site()
+build_home()
 
 #-----------------------------------------------------------------------
 
 # Install dependencies.
 system('sudo Rscript -e\\
        "library(devtools);\\
+        .libPaths(\\"/usr/lib/R/site-library\\");\\
         load_all();\\
         install_deps()"')
 
@@ -44,30 +49,24 @@ system('sudo Rscript -e\\
 #-----------------------------------------------------------------------
 # SCP transfer.
 
-# Transfere as vinhetas.
-cmd <- paste("scp -P 4922",
-             paste(dir(path = "inst/doc",
-                       pattern = "*.html",
-                       full.names = TRUE), collapse = " "),
-             "walmes@200.17.213.49:~/public_html/pacotes/EACS-vignettes")
-system(cmd)
-
 # IP address and port (you can define these credential in .Rprofile).
 credent <- scan(n = 2, what = "character")
 
 cmd <- sprintf(paste("rsync -avzp ./docs --progress",
                      '--rsh="ssh -p%s"',
                      '"%s:~/public_html/pacotes/gammacount"'),
-               credent[2], credent[1])
+               credent[2],
+               credent[1])
 
 cat(cmd)
 
 system(cmd)
 
 #-----------------------------------------------------------------------
-# https://stackoverflow.com/questions/42313373/r-cmd-check-note-found-no-calls-to-r-registerroutines-r-usedynamicsymbols
+# https://stackoverflow.com/questions/42313373/.
 
-sink("./src/gammacount_init.c")
+# Updates the *_init.c file.
+sink(file = "./src/gammacount_init.c")
 tools::package_native_routine_registration_skeleton(".")
 sink()
 
